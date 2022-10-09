a596b6766dd9520a890435e7a596943f2b121d4e
Faad Ragib <ragibfuad@gmail.com>
2022-10-09 12:21:20 +0600

    multiple reducers and combine them using combineReducers

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const redux = require("redux");
 const createStore = redux.createStore;
+const combineReducers = redux.combineReducers;
 
 const BUY_CAKE = "BUY_CAKE";
 const BUY_ICE_CREAMS = "BUY_ICE_CREAMS";
@@ -21,12 +22,15 @@ function buyIceCreams() {
 
 // (prevState, action) => newState
 
-const initialState = {
+const initialCakeState = {
 	numOfCakes: 10,
+};
+
+const initialIceCreamState = {
 	numOfIceCreams: 20,
 };
 
-const reducer = (state = initialState, action) => {
+const cakeReducer = (state = initialCakeState, action) => {
 	switch (action.type) {
 		case BUY_CAKE:
 			return {
@@ -34,6 +38,13 @@ const reducer = (state = initialState, action) => {
 				numOfCakes: state.numOfCakes - 1,
 			};
 
+		default:
+			return state;
+	}
+};
+
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+	switch (action.type) {
 		case BUY_ICE_CREAMS:
 			return {
 				...state,
@@ -45,7 +56,12 @@ const reducer = (state = initialState, action) => {
 	}
 };
 
-const store = createStore(reducer);
+const rootReducer = combineReducers({
+	cake: cakeReducer,
+	iceCream: iceCreamReducer,
+});
+
+const store = createStore(rootReducer);
 console.log("Initial state - ", store.getState());
 const unsubscribe = store.subscribe(() =>
 	console.log("Update state - ", store.getState())
